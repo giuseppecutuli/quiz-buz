775a831b4278f4451f0c54f3d6e944637f392157
Giuseppe Cutuli <giuseppe.cutuli@bizaway.com>
2025-10-15 15:48:07 +0200

    fix(ts): wrong definitions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import { RouterContext } from '@/types/router'
+import { RouterContext } from '@/lib/router'
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
