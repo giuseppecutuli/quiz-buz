7df82d0442b533ab91633edbd7cb4f0ce3987164
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2025-11-05 22:21:33 +0100

    feat: quiz card

---
+++ src/components/QuizCard/QuizCard.tsx
@@ -0,0 +1,52 @@
+import { Badge, Box, Card, HStack } from '@chakra-ui/react'
+
+type QuizCardProps = {
+  imageUrl: string
+  title: string
+  tags?: string[]
+  onClick?: () => void
+}
+
+export const QuizCard: React.FC<QuizCardProps> = ({ imageUrl, title, tags, onClick }) => {
+  return (
+    <Card.Root
+      h={350}
+      overflow="hidden"
+      maxW="xl"
+      bgPos="center"
+      bgSize="cover"
+      bgImage={'url(' + imageUrl + ')'}
+      cursor="pointer"
+      borderRadius="xl"
+      onClick={onClick}
+    >
+      <Card.Body>
+        <Box
+          mt="auto"
+          p="10px"
+          bg="rgba(0, 0, 0, 0.4)"
+          backdropFilter="blur(10px)"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          <Card.Title
+            fontSize="sm"
+            color="white"
+            fontWeight="normal"
+            lineClamp={2}
+          >
+            {title}
+          </Card.Title>
+        </Box>
+
+        <HStack position="absolute" top="3" left="3">
+          {tags?.map((tag, index) => (
+            <Badge key={index} colorScheme="teal">
+              {tag}
+            </Badge>
+          ))}
+        </HStack>
+      </Card.Body>
+    </Card.Root>
+  )
+}
--- src/routes/__auth.index.tsx
+++ src/routes/__auth.index.tsx
@@ -1,4 +1,7 @@
+import { Grid } from '@chakra-ui/react'
 import { createFileRoute } from '@tanstack/react-router'
+
+import { QuizCard } from '@/components/QuizCard'
 
 export const Route = createFileRoute('/__auth/')({
   component: DashboardPage,
@@ -8,6 +11,9 @@ function DashboardPage() {
   return (
     <section className="grid gap-2 p-2">
       <p>You are currently on the dashboard route.</p>
+      <Grid templateColumns="repeat(3, 1fr)" gap="6">
+        <QuizCard imageUrl="https://picsum.photos/200/300" title="Random title" />
+      </Grid>
     </section>
   )
 }
